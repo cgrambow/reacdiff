b97029407a16a0c3ab6329224344a603f3136174
Hongbo Zhao <hbozhao@mit.edu>
2019-07-01 19:58:58 -0400

    test another kernel

--- solver/DDFT.m
+++ solver/DDFT.m
@@ -20,13 +20,24 @@ y0 = y0(:);
 
 [k2,k] = formk(N,L);
 k2 = k2(:);
+kind1 = k{1}; kind2 = k{2};
+[kind1,kind2] = ndgrid(kind1(:),kind2(:));
+theta = angle(kind1+i*kind2);
+
+theta = theta(:);
+
 k0 = 10;
 alpha = 2;
 C2 = exp(-(sqrt(k2)-k0).^2/(2*alpha^2));
-J = -k2.*(-C2);
+C22 = C2;
+% C22 = C2 .* (1+cos(theta))/2;
+% C22 = C2 .* (exp(-(theta-pi/2).^2/(2*(50/180*pi)^2))+exp(-(theta+pi/2).^2/(2*(50/180*pi)^2)))/2;
+J = -k2.*(-C22);
 
-tspan = linspace(0,1.1,100);
+tspan = linspace(0,0.14,100);
+tspan = [0,1.1];
+% tspan = [0,0.14];
 options = odeset('AbsTol',1e-3);
 [tout,yout] = odeimex(@(t,y) DDFT_nlin(t,y,k2,N),J,tspan,y0);
 
-k2real(yout(:,end),N);
+figure; k2real(yout(:,end),N);
