912931b2e70783ebc49ac00f4ec44e60a950dd8a
Hongbo Zhao <hbozhao@mit.edu>
2019-07-31 01:20:02 -0400

    visualization of DDFT_nucleation23

--- solver/DDFT_nucleation23.m
+++ solver/DDFT_nucleation23.m
@@ -2,7 +2,7 @@
 %different initial condition
 %with discrete number of points
 addpath('../../CHACR/GIP')
-runoptim = true;
+runoptim = false;
 
 tic;
 L = [5,5];
@@ -53,7 +53,15 @@ resultpath = [largedatapath,'DDFT_nucleation23'];
 options = optimoptions('fminunc','OutputFcn', @(x,optimvalues,state) save_opt_history(x,optimvalues,state,resultpath));
 options = optimoptions(options,'HessianFcn','objective','Algorithm','trust-region','MaxFunctionEvaluations',10000,'MaxIterations',10000,'FunctionTolerance',1e-7);
 
-x_guess = [0,-9,0.5,0,-3];
-
-
-[x_opt,~,exitflag,params] = IP_DDFT(tdata,ydata,params,kernelSize,Cspace,options,x_guess,'Nmu',3,'discrete',true);
+if runoptim
+  x_guess = [0,-9,0.5,0,-3];
+  [x_opt,~,exitflag,params] = IP_DDFT(tdata,ydata,params,kernelSize,Cspace,options,x_guess,'Nmu',3,'discrete',true);
+else
+  modelfunc.C = @(k) exp(-(k-k0).^2/(2*alpha^2))*0.95;
+  modelfunc.mu = @(x) x - x.^2/2 + x.^3/3;
+  arg.C = linspace(0,2,500);
+  arg.mu = linspace(min(ydata(:)),max(ydata(:)),100);
+  history_production(resultpath,[1,21,38,1797],modelfunc,arg,tdata-tdata(1),ydata,params,kernelSize,Cspace,'IP_DDFT_arg',{'Nmu',3},'yyaxisLim',[-0.5,1.5;-1,1.2],'k0',k0,'xlim',[min(ydata(:)),2]);
+  f = gcf;
+  f.Position = [680 337 522 641];
+end
