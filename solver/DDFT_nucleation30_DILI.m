680b8da184b2551f82d333643893fd0a07d53c7f
Hongbo Zhao <hbozhao@mit.edu>
2020-05-17 02:51:52 -0400

    DDFT_nucleation30_DILI, fix bug, change back to normalized hermite function (not scaled by eigenvalue)

--- solver/DDFT_nucleation30_DILI.m
+++ solver/DDFT_nucleation30_DILI.m
@@ -48,28 +48,35 @@ Cspace = 'user';
 params.moreoptions = moreodeset('gmresTol',1e-5);
 
 %basis function here is hermitefunction scaled by 1/sqrt(eigenvalue), where eigenvalue is (2n+1) as in x^2 y(x) - y''_n(x) = (2n+1) y_n(x)
-customfunc = @(x) hermitefunction(x/k0,kernelSize,[],1) ./ sqrt(1:2:(2*kernelSize-1));
+% customfunc = @(x) hermitefunction(x/k0,kernelSize,[],1) ./ sqrt(1:2:(2*kernelSize-1));
 %get mu parameters at truth
 % xfit = linspace(0,3*k0,20)';
 % x_start = customfunc(xfit) \ modelfunc(xfit);
 %provide Csensval
+
+%I changed my mind, just use normalized hermitefunction, so prior is just exp(-||C(k)||_2^2/2)
+customfunc = @(x) hermitefunction(x/k0,kernelSize,[],1);
 k = sqrt(k2);
 params.Csensval = reshape(customfunc(k(:)), [size(k),kernelSize]);
 
-logpdf = @(x) IP_DDFT(tdata,ydata,params,kernelSize,Cspace,options,x,'Nmu',0,'discrete',true,'cutoff',k0,'mode','eval');
+logpdf = @(x) IP_DDFT(tdata,ydata,params,kernelSize,Cspace,[],x,'Nmu',0,'discrete',true,'cutoff',k0,'mode','eval');
 
-name = 'DDFT_nucleation30_DILI_MAP';
-resultpath = [largedatapath,name,'.mat'];
-if ~exist(resultpath,'file')
-  %find MAP
-  x_guess = zeros(1,kernelSize);
-  optoptions = optimoptions('fminunc','SpecifyObjectiveGradient',true,'Display','iter-detailed','MaxIterations',50,'StepTolerance',1e-6,'FunctionTolerance',1e-6,'HessianFcn','objective','Algorithm','trust-region');
-  optoptions = optimoptions(optoptions,'OutputFcn',@(x,optimvalues,state) save_opt_history(x,optimvalues,state,resultpath));
-  [x_start, Smin] = IP_DDFT(tdata,ydata,params,kernelSize,Cspace,options,x_guess,'Nmu',0,'discrete',true,'cutoff',k0);
-else
-  varload = load(resultpath);
-  x_start = varload.history(end,:);
-end
+% name = 'DDFT_nucleation30_DILI_MAP';
+% resultpath = [largedatapath,name,'.mat'];
+% if ~exist(resultpath,'file')
+%   %find MAP
+%   x_guess = zeros(1,kernelSize);
+%   optoptions = optimoptions('fminunc','SpecifyObjectiveGradient',true,'Display','iter-detailed','MaxIterations',50,'StepTolerance',1e-6,'FunctionTolerance',1e-6,'HessianFcn','objective','Algorithm','trust-region');
+%   optoptions = optimoptions(optoptions,'OutputFcn',@(x,optimvalues,state) save_opt_history(x,optimvalues,state,resultpath));
+%   [x_start, Smin] = IP_DDFT(tdata,ydata,params,kernelSize,Cspace,optoptions,x_guess,'Nmu',0,'discrete',true,'cutoff',k0);
+% else
+%   varload = load(resultpath);
+%   x_start = varload.history(end,:);
+% end
+
+%use result from DDFT_nucleation30 directly
+varload = load([largedatapath,'DDFT_nucleation30']);
+x_start = varload.history(end,:);
 
 options = [];
 options.init = x_start;
